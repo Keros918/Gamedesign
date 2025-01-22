ba58d7dbdfce89485c7193260d958ea90995bcea
Keros918 <haasleon.s@gmail.com>
2025-01-22 10:14:25 +0100

    maze view restrictions

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,12 +5,13 @@ public abstract class Interactable : MonoBehaviour
     [SerializeField] private SpriteRenderer interactSprite;
     [SerializeField] private Transform playerTransform;
     [SerializeField] private float interactDistance = 5f;
+    protected bool hasInteraction = true;
 
     void Update()
     {
         bool isWithinInteractDistance = IsWithinInteractDistance();
         interactSprite.gameObject.SetActive(isWithinInteractDistance);
-        if (Input.GetKeyDown(KeyCode.Q) && isWithinInteractDistance)
+        if (Input.GetKeyDown(KeyCode.Q) && isWithinInteractDistance && hasInteraction)
         {
             Interact();
         }
