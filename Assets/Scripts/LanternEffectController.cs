fc3dc6671838a957c65558cc9bbf59813f833f40
Keros918 <haasleon.s@gmail.com>
2025-01-22 02:01:18 +0100

    new tilemaps, get colliders working (more or less)

--- Assets/Scripts/LanternEffectController.cs
+++ Assets/Scripts/LanternEffectController.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class LanternEffectController : MonoBehaviour
 {
     [SerializeField] private RenderTexture furtwangenTexture;
     [SerializeField] private RenderTexture narugubiTexture;
+    [SerializeField] private Tilemap furtwangenCollider;
+    [SerializeField] private Tilemap narugubiCollider;
     [SerializeField] private Camera _camera;
     private bool isFurtwangenActive = true;
 
@@ -18,8 +21,8 @@ public class LanternEffectController : MonoBehaviour
 
     void Start()
     {
-        UpdateWorldTextures();
-        UpdateLanternEnabled();
+        UpdateWorldTextures(true);
+        UpdateLanternEnabled(true);
         aspectRatio = (float)furtwangenTexture.width / furtwangenTexture.height;
     }
     void Awake()
@@ -41,12 +44,12 @@ public class LanternEffectController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             isFurtwangenActive = !isFurtwangenActive;
-            UpdateWorldTextures();
+            UpdateWorldTextures(false);
         }
         if (playerControls.World.Action2.triggered)
         {
             isLanternEnabled = !isLanternEnabled;
-            UpdateLanternEnabled();
+            UpdateLanternEnabled(false);
         }
         Vector2 lanternPosition = new Vector2(lanternTransform.position.x, lanternTransform.position.y);
         Vector3 viewportPos = _camera.WorldToViewportPoint(lanternPosition);
@@ -56,7 +59,7 @@ public class LanternEffectController : MonoBehaviour
         lanternEffectMaterial.SetFloat("_AspectRatio", aspectRatio);
     }
 
-    void UpdateLanternEnabled()
+    void UpdateLanternEnabled(bool init)
     {
         if (isLanternEnabled)
         {
@@ -66,9 +69,24 @@ public class LanternEffectController : MonoBehaviour
         {
             lanternEffectMaterial.SetInt("_EnableLantern", 0);
         }
+        SwapColliders(init);
     }
 
-    void UpdateWorldTextures()
+    void SwapColliders(bool init)
+    {
+        if (init == true)
+        {
+            furtwangenCollider.gameObject.SetActive(true);
+            narugubiCollider.gameObject.SetActive(false);
+            return;
+        }
+        bool furtwangenColliderActive = furtwangenCollider.gameObject.activeSelf;
+        bool narugubiColliderActive = narugubiCollider.gameObject.activeSelf;
+        furtwangenCollider.gameObject.SetActive(!furtwangenColliderActive);
+        narugubiCollider.gameObject.SetActive(!narugubiColliderActive);
+    }
+
+    void UpdateWorldTextures(bool init)
     {
         if (isFurtwangenActive)
         {
@@ -80,5 +98,6 @@ public class LanternEffectController : MonoBehaviour
             lanternEffectMaterial.SetTexture("_ActiveWorldTexture", narugubiTexture);
             lanternEffectMaterial.SetTexture("_InactiveWorldTexture", furtwangenTexture);
         }
+        SwapColliders(init);
     }
 }
